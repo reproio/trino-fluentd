bc3034611afea872e652882be8d29985fca353c1
okkez <okkez000@gmail.com>
2024-02-09 16:33:38 +0900

    Stop using try with resource
    Because Fluency instance is auto-closable.

--- src/main/java/fluentd/FluentdListenerFactory.java
+++ src/main/java/fluentd/FluentdListenerFactory.java
@@ -20,11 +20,8 @@ public class FluentdListenerFactory implements EventListenerFactory {
         String fluentdHost = requireNonNull(map.get("event-listener.fluentd-host"), "event-listener.fluentd-host is null");
         String fluentdPort = requireNonNull(map.get("event-listener.fluentd-port"), "event-listener.fluentd-port is null");
         String fluentdTag = requireNonNull(map.get("event-listener.fluentd-tag"), "event-listener.fluentd-tag is null");
-        try (Fluency fluency = new FluencyBuilderForFluentd()
-                .build(fluentdHost, Integer.parseInt(fluentdPort))){
-            return new FluentdListener(fluency, fluentdTag);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Fluency fluency = new FluencyBuilderForFluentd()
+                .build(fluentdHost, Integer.parseInt(fluentdPort));
+        return new FluentdListener(fluency, fluentdTag);
     }
 }
